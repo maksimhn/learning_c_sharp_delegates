1a3bfbdf0ecc818da5765180d15f39349b076640
Maksim Hranin <maksim.hranin@slalom.com>
2015-12-03 15:31:10 -0500

    Add temporary delegate, event handler and event method.

--- console_app/Program.cs
+++ console_app/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace console_app
 {
+
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
     class Program
     {
         static void Main(string[] args)
         {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            DoWork(del1);
+
+            Console.ReadLine();
         }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GenerateReports);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("Work 1 Performed");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("Work 2 Performed");
+        }
+    }
+
+    public enum WorkType
+    {
+        GotToMeetings,
+        Golf,
+        GenerateReports
     }
 }
