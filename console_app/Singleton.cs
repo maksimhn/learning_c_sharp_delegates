b1c900d27cdb48d1d65106c23b45815d3d4db6ed
maksimhn <maksimhranin@gmail.com>
2015-12-04 14:54:07 -0500

    Add test class Singleton

---
+++ console_app/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    // "sealed" means it cannot be overwritten
+    public sealed class Mediator
+    {
+        // we will only allow one instance created
+        private static readonly Mediator _Instance = new Mediator();
+
+        // and in order to forbid creation of new instances we will make contructor do nothing
+        private Mediator() { }
+
+        // only one way to get it instantiated is to call this method
+        public static Mediator GetInstance()
+        {
+            return _Instance;
+        }
+
+        // Instance functionality
+    }
+}
